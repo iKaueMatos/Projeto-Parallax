496187df341df6f38d1056b7a1fedfb0ed83363f
Kaue Matos <98132837+iKaueMatos@users.noreply.github.com>
2022-03-27 15:27:18 -0300

    Add files via upload

---
+++ projeto/js/main.js
@@ -0,0 +1,14 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+
+window.addEventListener('scroll', function() {
+
+    let value = window.scrollY;
+    stars.style.left = value + 'px';
+
+
+})
